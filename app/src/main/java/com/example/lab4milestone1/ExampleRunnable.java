8b90dff8c68ee8a41f84e107455386f9c1f5c656
Peter <pswanson2@wisc.edu>
2020-02-19 15:39:38 -0600

    Initial commit

---
+++ app/src/main/java/com/example/lab4milestone1/ExampleRunnable.java
@@ -0,0 +1,15 @@
+package com.example.lab4milestone1;
+
+public class ExampleRunnable implements Runnable {
+
+    int seconds;
+
+    ExampleRunnable(int seconds) {
+        this.seconds = seconds;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
--- app/src/main/java/com/example/lab4milestone1/MainActivity.java
+++ app/src/main/java/com/example/lab4milestone1/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.lab4milestone1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonStartThread;
+
+    private volatile boolean stopThread = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonStartThread = findViewById(R.id.button1);
+    }
+
+    public void startThread(View view) {
+        stopThread = false;
+        ExampleRunnable runnable = new ExampleRunnable(10);
+        new Thread(runnable).start();
+    }
+
+    public void stopThread(View view) {
+        stopThread = true;
     }
 }
